56da6c42e66326125bdf08f7df663cd87f4ac330
inbum <1234!@>
2022-04-18 23:29:39 +0900

    로그인, 회원가입 페이지 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import {ThemeProvider} from "styled-components";
-import {StatusBar} from "react-native";
+import {StatusBar, Image} from "react-native";
+import {useState} from "react";
+import {Asset} from 'expo-asset';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import {theme } from './theme';
 
+/**
+ * images을 매개변수로 받는다. map 함수를 사용하여 image가 string type이면 prefetch로 반환
+ * @param images
+ * @returns {*}
+ */
+const cacheImages = images => {
+    return images.map(image => {
+        if (typeof image === 'string') {
+            return Image.prefetch(image);
+        } else {
+            return Asset.fromModule(image).downloadAsync();
+        };
+    });
+}
 
-const App = () => {
-    return (
-        <ThemeProvider theme={theme} >
-            <StatusBar barStyle='dark-content' />
-        </ThemeProvider>
-    );
+const cacheFonts = fonts => {
+    return fonts.map(font => Font.loadAsync(font));
 };
 
+    const App = () => {
+        const [isReady, setIsReady] = useState(false);
+
+        const _loadAssets = async () => {
+            const imageAssets = cacheImages([require('../assets/splash.png')]);
+            const fontAssets = cacheFonts([]);
+
+            await Promise.all([...imageAssets, ...fontAssets]);
+        };
+        return isReady ? (
+            <ThemeProvider theme={theme}>
+                <StatusBar barStyle='dark-content'/>
+            </ThemeProvider>
+        ) : (
+            <AppLoading
+                startAsync={_loadAssets}
+                onFinish={() => setIsReady(true)}
+                onError={console.warn}
+            />
+        );
+    };
 export default App;
---
+++ src/utils/firebase.js
@@ -0,0 +1,4 @@
+import * as firebase from "firebase/compat";
+import config from '../../firebase.json';
+
+const app = firebase.initializeApp(config);
